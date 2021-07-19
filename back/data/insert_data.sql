64aa8a9952c9cb8e7f6363e19b133e8f230fd907
Etudiant O'clock <remy.bregeon@free.fr>
2021-07-19 10:59:40 +0200

    Creation of models

--- back/data/insert_data.sql
+++ back/data/insert_data.sql
@@ -1,6 +1,10 @@
 
-INSERT INTO "farmer" ("id", "firstname","name","location","biography","basket","admin_id") VALUES
-(1, 'Thierry','Vallarino','Maraîcher à la Cadière d’Azur','A La Cadière d’Azur où il est un des derniers maraîchers bio, il lui faut pugnacité et passion pour persévérer entre les aléas de la météo, les prédateurs ailés ou à grandes oreilles, la convoitise immobilière et l’expansionnisme viticole.
+INSERT INTO "admin" ("email","mdp") VALUES
+('remy.bregeon@free.fr','toto');
+
+
+INSERT INTO "farmer" ("firstname","name","location","biography","basket","admin_id") VALUES
+('Thierry','Vallarino','Maraîcher à la Cadière d’Azur','A La Cadière d’Azur où il est un des derniers maraîchers bio, il lui faut pugnacité et passion pour persévérer entre les aléas de la météo, les prédateurs ailés ou à grandes oreilles, la convoitise immobilière et l’expansionnisme viticole.
 Thierry Vallarino exploite depuis trois décennies deux hectares au cœur de l’AOP Bandol. Soucieux de la terre, de la qualité de ses produits il n’a de cesse de chercher à faire évoluer ses pratiques entre demande des amapiens et pragmatisme de leur mise en culture (rotation des cultures, jachères pour le repos des sols, culture sous serre ou de plein champs, temps d’occupation du sol par production…).
 En toute saison il fournit des légumes dont la qualité gustative et la fraicheur ravissent nos papilles. Quelques fraises peuvent venir compléter le panier à la belle saison.','Composition type de paniers:
 
@@ -26,14 +30,38 @@ courge
 épinard ou blette
 salade
 persil',1),
-(3, 'Sylvain','Apostolo','Le Jas du Vignal','La ferme du Jas du Vignal se situe à Sillans-la-cascade, petit village du haut-var entre Draguignan et Brignoles. 
+
+('Sylvain','Apostolo','Le Jas du Vignal','La ferme du Jas du Vignal se situe à Sillans-la-cascade, petit village du haut-var entre Draguignan et Brignoles. 
 « Les 200 brebis de race Lacaune pâturent dans les collines et les prés toute l’année suivant les conditions climatiques et nous offrent un lait onctueux, aromatique et délicat.
 Le lait récolté est transformé par nos soins en fromages fermiers certifiés en agriculture biologique.
 Les brebis sont traites tous les matins de septembre à juin avant d’aller se dégourdir les pattes et se remplir la panse dans nos prairies. 
 Durant la période estivale, c’est repos pour tout le monde! Les brebis chaument à l’ombre des arbres et dégustent du Sorgho semé par nos soins. 
 Et nous, paysans du Jas du Vignal, nous accordons un peu de repos entre les récoltes de foin, qui serviront à l’alimentation du troupeau l’année suivante ! » S Apostolo
 ','',1),
-(7, 'Bertrand',''),
-(4, 'La légende continue.'),
-(5, 'Patrimoine exporté dans le monde entier'),
-(6, 'Près de 1000 fromages différents');
+
+('John','Doe','Chevrier à La Roquebrussane EARL Les Vallons','Élevage extensif de chèvres en système sylvopastoral : les animaux pâturent dans les collines du massif de la Sainte Baume. Le troupeau profite ainsi des ressources fourragères et participe à la prévention des incendies en entretenant les sous-bois. Cette pratique donne un lait parfumé et riche.
+Leurs animaux sont de races rustiques (chèvres du Rove et alpines chamoisées) ce qui leur permet de rester à l’extérieur toute l’année. Les Carel produisent des fromages fermiers : les matières premières sont exclusivement issues de l’exploitation et transformées sur place dans une fromagerie qui répond aux normes européennes.
+Des fromages de chèvre.','Des fromages de chèvre. Leurs spécialités sont :
+•Le pèbre d’aï : un crottin de chèvre de 100 gr On peut le consommer frais, à partir de 5 jours, ou affiné, de 10 à 30 jours. Il est aromatisé d’un brin de sarriette (pèbre d’aï)
+
+•La ravigotte : un fromage de chèvre à tartiner, aromatisé vec de l’ail, du basilic et de l’huile d’olive.
+
+•La tomme de la Sainte Baume : une pâte pressée, type tomme de Savoie. Elle est affinée au minimum un mois',1),
+
+('Bertrand','Allais','Céréalier, meunier','Entre Bras et Châteauvert se trouve le domaine de la Rouvière plane où Bertrand Allais cultive céréales et légumineuses depuis 1977. Converti à l’agriculture biologique depuis l’an 2000, sur les 70 hectares de son exploitation, il en cultive 60, en laisse 10 en jachères. L’irrigation se fait par le canal de l’Argens. 
+Le compost à base de fumier récupéré chez des éleveurs biologiques et de temps à autre un engrais azoté viennent nourrir ses terres déjà enrichies en azote par la culture de légumineuses telles que luzerne et sainfoin.','Il choisit de cultiver d’anciennes variétés de céréales plus adaptées au climat, plus goûteuses mais forcément moins productives : blé tendre, blé des Pharaons dit Kamut, petit épeautre, seigle… à partir desquelles il fabrique ses farines. Il produit également des pois chiches et des lentilles vertes.',1),
+
+('Vincent','Mélé','Les Sagnes, Saint Julien du Gua, Ardèche','Producteur de châtaignes, de farine de châtaignes, de confitures… Vincent Mélé est installé à Saint Julien du Gua, Ardèche, depuis 2015. 
+Par la localisation de l’exploitation dans le Parc Régional des Monts d''Ardèche, ses châtaignes bénéficient de l''Appellation d''origine protégée (AOP).
+Cette AOP est réservée aux châtaignes de variétés locales anciennes ardéchoises, cultivées en vergers de type traditionnels. Depuis 2016, tous ses châtaigniers sont en reconversion en l''agriculture biologique.
+
+Son engagement principal est de produire des fruits de grande qualité. Les quantités restent donc modestes. La production de châtaignes est de l''ordre de 3 tonnes par saison (d''octobre à novembre). 
+','Vincent Mélé travaille en famille de façon artisanale. Tout est question de respect de la châtaigneraie et du produit. 
+
+•Le ramassage des châtaignes se fait au sol à la main avec un seau et une forcole. La main d''œuvre est familiale. Seules les plus belles châtaignes sont ramassées, les plus petits fruits sont laissés au sol pour nourrir les brebis. 
+•Le tri des châtaignes est aussi réalisé à la main (calibre et variété). Puis elles sont trempées dans une grande bassine d''eau pour éliminer les châtaignes véreuses. Les autres sont mises à sécher sur une grille pour les conditionner plus tard.
+
+
+Les principales variétés de châtaignes cultivées sont : La Comballe (ancienne variété de châtaigne très gustative et légèrement sucrée), La Bouche Rouge et la Garinche
+La farine de châtaigne est une farine sans gluten-.
+La confiture de châtaigne est dite extra car c''est le produit le plus concentré en purée de châtaigne (minimum 45 % de purée de châtaigne) par rapport à une crème de marron (38 %) et une confiture de châtaigne (35 %)',1);
