f5d08104f0b7c47acfa4fcd59b97b7547bf4f063
davidegloh <davidegloh@protonmail.com>
2021-07-19 16:49:56 +0200

    affichage 4 blocs recette avec image titre et description en vert

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -1,11 +1,13 @@
 // == Import npm
 import React from 'react';
+import Recette from '../Recettes/Recette';
 
 // == Import
 // == Composant
 const App = () => (
   <div className="app">
     <h1>hello world!</h1>
+    <Recette/>
   </div>
 );
 
---
+++ front/src/components/Recettes/Recette/index.js
@@ -0,0 +1,48 @@
+import React from 'react'; 
+import './styles.scss';
+
+
+function Recette(){
+
+    return(
+
+        
+        <div>
+
+
+            <div class="card-recette">
+                <div class="photo-recette"></div>
+                <h1 class="titre-recette">Titre recette</h1>
+                <h2 class="description-recette">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat nostrum doloribus culpa dicta. Ipsa quaerat maxime, cum debitis voluptatum natus explicabo, enim aut, quos similique eveniet doloremque nobis corporis aspernatur?</h2>
+            </div>
+
+            <div class="card-recette">
+                <div class="photo-recette"></div>
+                <h1 class="titre-recette">Titre recette</h1>
+                <h2 class="description-recette">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni ad, officiis voluptatum in ipsa quas, architecto corporis sapiente aperiam, recusandae enim harum et repellendus eveniet maxime earum laborum quisquam quibusdam!</h2>
+            </div>
+            <div class="card-recette"> 
+                <div class="photo-recette"></div>
+                <h1 class="titre-recette">Titre recette</h1>
+                <h2 class="description-recette">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores accusantium aperiam quis quam fugiat placeat ducimus porro, magni ad quos. Ad natus sequi eaque provident id ea tempore dolore praesentium!</h2>
+            </div>
+            <div class="card-recette"> 
+                <div class="photo-recette"></div>
+                <h1 class="titre-recette">Titre recette</h1>
+                <h2 class="description-recette">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores accusantium aperiam quis quam fugiat placeat ducimus porro, magni ad quos. Ad natus sequi eaque provident id ea tempore dolore praesentium!</h2>
+            </div>
+
+
+        </div>
+        
+
+    )
+
+
+
+}
+
+
+
+
+export default Recette;
