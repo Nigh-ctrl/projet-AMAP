4b0b70d4235a3776dc04840a7939961ee5557214
Thibault <NighDev@gmail.com>
2021-07-20 16:43:55 +0200

    simplification du footer

--- front/src/components/Footer/index.js
+++ front/src/components/Footer/index.js
@@ -4,7 +4,6 @@ import './style.scss';
 
 const Footer =  () => (
   <footer>
-  <div className='left'>apothéose o'clock 2021</div>
   <div className='center'>mentions légales</div>
   <div className='right'>connexion</div>
   </footer>
