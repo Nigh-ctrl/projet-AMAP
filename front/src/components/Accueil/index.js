b9ee338f206422aa50ad35efa7eb2458a5b71d5c
Thibault <NighDev@gmail.com>
2021-07-20 14:41:36 +0200

    début responsive composant article

--- front/src/components/Accueil/index.js
+++ front/src/components/Accueil/index.js
@@ -4,6 +4,21 @@ import './style.scss'
 
 const Accueil = () => (
   <div className="accueil">
+    <div className="">
+      <div className="text">présentation de l'amap</div>
+    </div>
+    <div className="">
+      <div className="text">nos producteurs</div>
+    </div>
+    <div className="">
+      <div className="text">calendrier de livraison</div>
+    </div>
+    <div className="">
+      <div className="text">actu de l'amap</div>
+    </div>
+    <div className="">
+      <div className="text">recettes</div>
+    </div>
   </div>
 )
 
--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -6,13 +6,11 @@ import './styles.scss'
 import Header from '../Header'
 import Accueil from '../Accueil'
 import Footer from '../Footer'
-import Agriculteurs from '../Agriculteurs'
-import Articles from '../Articles'
 // == Composant
 const App = () => (
   <div className="app">
     <Header/>
-    <Articles />
+    <Accueil />
     <Footer/>
   </div>
 );
