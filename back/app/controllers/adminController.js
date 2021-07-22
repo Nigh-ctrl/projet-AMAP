df0e469ddc42fb227c9484d0a7bbc39e36faf5e5
Etudiant O'clock <remy.bregeon@free.fr>
2021-07-22 16:26:35 +0200

    login_jwt

---
+++ back/app/controllers/adminController.js
@@ -0,0 +1,46 @@
+//We call here the class we needed
+const Admin = require('../models/admin');
+const jwtToken=require('../utilities/jwt');
+
+const adminController={
+
+    loginSubmit: async (request, response) => {
+        // s'authentifier
+        
+        try{
+            const email = request.body.email;
+            const password = request.body.password;
+
+            const user = await Admin.findOne(email);
+
+
+            if(!user) {
+                // l'utilisateur n'éxiste pas
+                // on s'arrte là (return) affiche la page de login avec l'erreur
+                return response.status(500).json({"error":"Cet admin n'existe pas"});
+            }
+            
+            // si on arrive jusqu'ici c'est que l'utilisateur existe
+            //const isPasswordValid = bcrypt.compareSync(password, user.password);
+            const isPasswordValid = password;
+
+            if(isPasswordValid===user.password) {
+                return response.status(200).json({
+                    "adminId":user.id,
+                    "token":jwtToken.generateTokenForAdmin(user)
+                });
+            }else{
+                // si le mot de passe ne correspond pas a celui qui à été encrypté en BDD alors on reaffiche la page de login avec une erreur
+                return response.status(403).json({"error":"Mot de passe invalide"});
+            }
+
+
+        }catch(error){
+            response.status(500).json(error.message);
+        }
+    
+    },
+
+}
+
+module.exports = adminController;
---
+++ back/app/middlewares/checkAdmin.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+const checkAdminMiddleware=(request, response,next)=>{
+    const authHeader=request.headers['authorization'];
+    const token =authHeader && authHeader.split(' ')[1];
+    
+    if(token===null){
+        return response.status(401);
+    }
+    
+    jwt.verify(token,process.env.JWT_SIGN_SECRET,(err,user)=>{
+        if(err){
+            response.status(403).json({"error":"Vous n'avez pas les droits"});
+        }else{
+            next();
+        }
+    })
+
+}
+
+module.exports=checkAdminMiddleware;
