4f3e7d2a56e01c3050821a8de3d83ed3282a09d5
Thibault <NighDev@gmail.com>
2021-07-23 18:24:42 +0200

    fix d'une erreur lors de la connexion

--- front/src/components/Connexion/index.js
+++ front/src/components/Connexion/index.js
@@ -1,43 +1,38 @@
 import React, {useState, useEffect} from 'react';
 import 'semantic-ui-css/semantic.min.css'
 import { Button, Input } from 'semantic-ui-react';
-import './styles.scss'
 import axios from 'axios'
+
+import './styles.scss'
 
 const Connexion = () => {
 
   const [logged, setLogged] = useState(false)
 
- const [email, setEmail ] = useState("");
- const [password, setPassword] = useState("");
- 
- 
+  const [email, setEmail ] = useState("");
+  const [password, setPassword] = useState("");
+
+
   function handleSubmit (event) {
-    event.preventDefault()
-    const token = email + password
-    console.log("event")
-    axios({
-      method: 'post',
-      url: 'http://localhost:4000/login',
-      data: {
-        email: email,
-        password: password
-      }
-    }, { headers: {'Authorization': `Bearer ${token}`}} )
-    .then((res) => {
-      console.log(res.data);
-      setPassword(res.data.password);
-      setEmail(res.data.email);
-      setLogged(true)
-      if(logged){
-        return(
-          <Redirect to="/" />
-          )
-        };
-    })
-    .catch((e) => {
-      console.log("erreur lors du login", e);
-    })
+      // annule le refresh de la page
+      event.preventDefault()
+      const token = email + password
+      //requete axios
+      axios({
+        method: 'post',
+        url: 'http://localhost:4000/login',
+        data: {
+          email: email,
+          password: password
+        }
+      }, { headers: {'Authorization': `Bearer ${token}`}} )
+      .then((res) => {
+        console.log(res.data);
+        setLogged = true
+      })
+      .catch((e) => {
+        console.log("erreur lors du login", e);
+      })
   }
 
   return (
@@ -50,21 +45,19 @@ const Connexion = () => {
               onChange={(e) => setEmail(e.target.value)}
               value={email}
               labelPosition='right'
-              placeholder='Ex: CamLag@'
             />
           </div>
           <div className="admin-login-password">
             <Input icon='lock'
               value={password}
               onChange={(e)=> setPassword(e.target.value)}
-              placeholder='****' />
+               />
           </div>
           <div className="admin-login-submit">
               <Button  type="submit">Se connecter</Button>
           </div>
         </form>
       </div>
-
     </section>
   )
 };
