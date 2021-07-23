977fb57182dcd41f762a29f70c963e6c6a549e8f
Camille Lagorce <lecraot24@gmail.com>
2021-07-23 09:13:50 +0200

    Commit vide

--- front/src/components/Connexion/index.js
+++ front/src/components/Connexion/index.js
@@ -1,25 +1,40 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import 'semantic-ui-css/semantic.min.css'
-import { Dropdown, Button, Input } from 'semantic-ui-react';
+import { Button, Input } from 'semantic-ui-react';
 import './styles.scss'
 import axios from 'axios'
 
 const Connexion = () => {
- const [email, setEmail ] = useState();
- const [password, setPassword] = useState();
+  
+  const [email, setEmail ] = useState();
+  const [password, setPassword] = useState();
 
- function handleSubmit(event){
-   event.preventDefault()
-   console.log(event)
-   console.log(email, password)
-   //TODO requete axios, envoyer les vals dans le body,
-   //.then()catch()
-   //axios.get('EndpointWithAuthorizedError')
-    //.then((response) => {})
-    //.catch((error) => {
-    //  console.log(error);
-   // })
+  function handleSubmit(event){
+    event.preventDefault();
+    console.log(event)
+    console.log(email, password)
+    //TODO requete axios, envoyer les vals dans le body,
   }
+  const getAdmins = () => {
+    axios({
+      method: 'post',
+      url: 'http://localhost:5000/login',
+      data: {
+        email: email,
+        password: password
+      }
+    })//, { headers: {'Authorization': `Bearer ${token}`}} 
+    .then((res) => {
+      console.log(res.data);
+      setPassword(res.data.password);
+      setEmail(res.data.email);
+    })
+    .catch((e) => {
+      console.log("erreur lors du login", e);
+    })
+  }  
+  
+  useEffect(getAdmins, []);  
 
   return (
     <section className="connexion">
@@ -28,7 +43,6 @@ const Connexion = () => {
         <form className="admin-login-form" onSubmit={handleSubmit} >
           <div className="admin-login-email">
             <Input
-
               onChange={(event) => setEmail(event.target.value)}
               value={email}
               labelPosition='right'
@@ -36,9 +50,11 @@ const Connexion = () => {
             />
           </div>
           <div className="admin-login-password">
-            <Input icon='lock'
+            <Input
+              icon='lock'
               onChange={(event)=> setPassword(event.target.value)}
-              placeholder='****' />
+              placeholder='****' 
+              value={password}/>
           </div>
           <div className="admin-login-submit">
               <Button  type="submit">Se connecter</Button>
