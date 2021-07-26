d5e789d0bc113d7d50a14e3668a4ec575b2bc0b4
Camille Lagorce <lecraot24@gmail.com>
2021-07-26 16:58:16 +0200

    getAllArticles done

--- front/src/components/Articles/index.js
+++ front/src/components/Articles/index.js
@@ -1,45 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 // import PropTypes from 'prop-types';
 import NavBarre from './NavBarre';
+import axios from 'axios';
 
 import './styles.scss';
 
-const Articles = () => (
-  <section className="articles">
-    <h1 className="page-title">Articles</h1>
-    {/* Ajout de NavLink pour indiquer quelle est la page affichée (activeClassName)*/}
-    <NavBarre />
-    <div className="articles-list">
-      {/* Ici un map lors de la dynamisation */}
-      <article className="articles-card">
-        <h3 className="articles-title">Panier de la semaine 19/07 </h3>
-        <img className="articles-img" src="https://thumbs.dreamstime.com/z/panier-en-osier-avec-les-l%C3%A9gumes-organiques-crus-assortis-dans-le-jardin-48896220.jpg" alt=""/>
-        <a className="articles-read-more" href="">En savoir plus</a>
-      </article>
-      {/* fin du map */}
-      <article className="articles-card">
-        <h3 className="articles-title">Panier de la semaine 19/07 </h3>
-        <img className="articles-img" src="https://thumbs.dreamstime.com/z/panier-en-osier-avec-les-l%C3%A9gumes-organiques-crus-assortis-dans-le-jardin-48896220.jpg" alt=""/>
-        <a className="articles-read-more" href="">En savoir plus</a>
-      </article>
-      <article className="articles-card">
-        <h3 className="articles-title">Panier de la semaine 19/07 </h3>
-        <img className="articles-img" src="https://thumbs.dreamstime.com/z/panier-en-osier-avec-les-l%C3%A9gumes-organiques-crus-assortis-dans-le-jardin-48896220.jpg" alt=""/>
-        <a className="articles-read-more" href="">En savoir plus</a>
-      </article>
-      <article className="articles-card">
-        <h3 className="articles-title">Panier de la semaine 19/07 </h3>
-        <img className="articles-img" src="https://thumbs.dreamstime.com/z/panier-en-osier-avec-les-l%C3%A9gumes-organiques-crus-assortis-dans-le-jardin-48896220.jpg" alt=""/>
-        <a className="articles-read-more" href="">En savoir plus</a>
-      </article>
-      <article className="articles-card">
-        <h3 className="articles-title">Panier de la semaine 19/07 </h3>
-        <img className="articles-img" src="https://thumbs.dreamstime.com/z/panier-en-osier-avec-les-l%C3%A9gumes-organiques-crus-assortis-dans-le-jardin-48896220.jpg" alt=""/>
-        <a className="articles-read-more" href="">En savoir plus</a>
-      </article>
-    </div>
-  </section>
-);
+const Articles = () => {
+
+  const [articles, setArticles] = useState([]);
+  
+  const getArticles = () => (
+    //requete axios
+    axios({
+      method: 'get',
+      url: 'http://localhost:5000/articles',
+      data: {
+        articles: articles
+      }
+    })
+    .then((res) => {
+      console.log(res.data);
+      setArticles(res.data);
+    })
+    .catch((e) => {
+      console.log("erreur lors du login", e);
+    })
+  )
+
+  useEffect(getArticles, []);
+
+  return(
+    <section className="articles">
+      <h1 className="page-title">Articles</h1>
+      <NavBarre />
+      <div className="articles-list">
+        {/* Ici un map lors de la dynamisation */}
+        {
+          articles.map((article) => (
+            <article key={article.id+article.title} className="articles-card">
+              <h3 className="articles-title">{article.title}</h3>
+              <img className="articles-img" src="https://thumbs.dreamstime.com/z/panier-en-osier-avec-les-l%C3%A9gumes-organiques-crus-assortis-dans-le-jardin-48896220.jpg" alt=""/>
+              <p className="articles-content">{article.content}</p>
+              <a className="articles-read-more" href="">En savoir plus</a>
+            </article>
+          ))
+        }
+        {/* fin du map */}
+      </div>
+    </section>
+  )
+};
 
 // Content.propTypes = {
 // 
