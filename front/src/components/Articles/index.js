dd1c4dc5ba31ffc7e7b7992254d9b9a8998decab
Camille Lagorce <lecraot24@gmail.com>
2021-07-29 17:04:01 +0200

    getByCategoy done

--- front/src/components/Articles/index.js
+++ front/src/components/Articles/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-// import PropTypes from 'prop-types';
 import NavBar from './NavBar';
 import axios from 'axios';
 import { Link, useParams } from 'react-router-dom';
@@ -19,22 +18,25 @@ const Articles = () => {
       url: 'http://localhost:5000/articles'
     })
     .then((res) => {
-      console.log(res.data);
-      setArticles(res.data);
+      if(category === undefined){
+        setArticles(res.data);
+        return
+      }
+      const articlesFiltre = res.data.filter(article => (article.category_id == category));
+      setArticles(articlesFiltre);
     })
     .catch((e) => {
-      console.log("erreur lors du login", e);
+      console.log("erreur lors de la requete articles", e);
     })
   )
 
-  useEffect(getArticles, []);
+  useEffect(getArticles, [category]);
 
   return(
     <section className="articles">
       <h1 className="page-title">Actu de l'amap</h1>
       <NavBar />
       <div className="articles-list">
-        {/* Ici un map lors de la dynamisation */}
         {
           // on ne garde que le component Evenements qui devra etre renommé en Article
           articles.map((article) => (
@@ -48,18 +50,9 @@ const Articles = () => {
             </Link>
           ))
         }
-        {/* fin du map */}
       </div>
     </section>
   )
 };
-
-// Content.propTypes = {
-// 
-// };
-
-// Content.defaultProps = {
-//   
-// };
 
 export default Articles;
