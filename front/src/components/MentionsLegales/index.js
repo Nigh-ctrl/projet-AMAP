a60c741f29aac615c24a3dac3cad6815fe434ced
davidegloh <davidegloh@protonmail.com>
2021-08-02 12:39:58 +0200

    page mentions-légales

---
+++ front/src/components/MentionsLegales/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+import './styles.scss';
+
+const MentionsLegales = () => (
+  <section className="mentionsLegales">
+      <h1 className="page-title">Mentions Légales</h1>
+      <div className="mentionsLegales-div">
+        
+        <p className="mentionsLegales-information">
+        
+          Nom : AMAP La Seyne sur Mer <br/>
+          Contact : <br/>
+          Hebergement : <br/>
+
+
+        </p>
+      </div>
+      <div className="contact-api-div">
+      
+      </div>
+  </section>
+);
+
+
+// Content.propTypes = {
+// 
+// };
+
+// Content.defaultProps = {
+//   
+// };
+
+export default MentionsLegales;
