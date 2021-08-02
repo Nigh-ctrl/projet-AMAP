48fa090fa052643d04209effa37743b501e8531a
Camille Lagorce <lecraot24@gmail.com>
2021-08-02 15:30:36 +0200

    bdd

--- back/data/insert_data.sql
+++ back/data/insert_data.sql
@@ -10,30 +10,27 @@ DELETE FROM "season";
 INSERT INTO "farmer" ("slug","firstname","name","location","biography","basket","admin_id") VALUES
 ('Thierry_Vallarino','Thierry','Vallarino','Maraîcher à la Cadière d’Azur','A La Cadière d’Azur où il est un des derniers maraîchers bio, il lui faut pugnacité et passion pour persévérer entre les aléas de la météo, les prédateurs ailés ou à grandes oreilles, la convoitise immobilière et l’expansionnisme viticole.
 Thierry Vallarino exploite depuis trois décennies deux hectares au cœur de l’AOP Bandol. Soucieux de la terre, de la qualité de ses produits il n’a de cesse de chercher à faire évoluer ses pratiques entre demande des amapiens et pragmatisme de leur mise en culture (rotation des cultures, jachères pour le repos des sols, culture sous serre ou de plein champs, temps d’occupation du sol par production…).
-En toute saison il fournit des légumes dont la qualité gustative et la fraicheur ravissent nos papilles. Quelques fraises peuvent venir compléter le panier à la belle saison.','Composition type de paniers:
-
-Contrat été (de mai à octobre)
-Tomates longues, rondes, jaunes rouges…,
-Courgettes longues, rondes, jaune ou vertes,
-Aubergines (blanches ou violettes),
-Poivrons,
-Melons,
-Pomme de terre,
-Celeri rave,
-Salade,
-Basilic,
-
-Contrat hiver (novembre à Avril)
-Carottes fanes,
-pomme de terre,
-poireaux,
-brocolis,
-navet,
-choux variés,
-courge,
-épinard ou blette,
-salade,
-persil',1),
+En toute saison il fournit des légumes dont la qualité gustative et la fraicheur ravissent nos papilles. Quelques fraises peuvent venir compléter le panier à la belle saison.','Contrat été (de mai à octobre) ,
+Tomates (longues, rondes, jaunes rouges…) ,
+Courgettes (longues, rondes, jaune ou vertes) ,
+Aubergines (blanches ou violettes) ,
+Poivrons ,
+Melons ,
+Pomme de terre ,
+Celeri rave ,
+Salade ,
+Basilic ,
+Contrat hiver (novembre à Avril) ,
+Carottes fanes ,
+Pomme de terre ,
+Poireaux ,
+Brocolis ,
+Navet ,
+Choux variés ,
+Courge ,
+Épinard ou blette ,
+Salade ,
+Persil',1),
 
 ('Theodore_Planas-Rastoin','Théodore','Planas-Rastoin','Meyreuil','Ce jeune paysan-boulanger de 30 ans a plus d’une corde à son arc ; il cultive ses propres céréales, pétrit manuellement un pain bio à base de blés de variétés anciennes.
 Passionné d’authenticité et respectueux de biodynamie, Théodore fait revivre les terres familiales,  y plante des céréales et installe un vignoble qu’il travaille à la main dans le plus strict respect des règles Bio et laboure avec un cheval.
@@ -43,7 +40,7 @@ Mais quel régal pour nos papilles.
 
 On parle d''eux
 https://www.pressreader.com/france/la-revue-du-vin-de-france/20150213/283948881028213
-https://domaine-rochefontaine.com/actualite/','',1),
+https://domaine-rochefontaine.com/actualite/','Farine , Pain',1),
 
 ('Sylvain_Apostolo','Sylvain','Apostolo','Le Jas du Vignal','La ferme du Jas du Vignal se situe à Sillans-la-cascade, petit village du haut-var entre Draguignan et Brignoles. 
 « Les 200 brebis de race Lacaune pâturent dans les collines et les prés toute l’année suivant les conditions climatiques et nous offrent un lait onctueux, aromatique et délicat.
@@ -51,16 +48,12 @@ Le lait récolté est transformé par nos soins en fromages fermiers certifiés 
 Les brebis sont traites tous les matins de septembre à juin avant d’aller se dégourdir les pattes et se remplir la panse dans nos prairies. 
 Durant la période estivale, c’est repos pour tout le monde! Les brebis chaument à l’ombre des arbres et dégustent du Sorgho semé par nos soins. 
 Et nous, paysans du Jas du Vignal, nous accordons un peu de repos entre les récoltes de foin, qui serviront à l’alimentation du troupeau l’année suivante ! » S Apostolo
-','',1),
+','Fromages',1),
 
 ('John_Doe','John','Doe','Chevrier à La Roquebrussane EARL Les Vallons','Élevage extensif de chèvres en système sylvopastoral : les animaux pâturent dans les collines du massif de la Sainte Baume. Le troupeau profite ainsi des ressources fourragères et participe à la prévention des incendies en entretenant les sous-bois. Cette pratique donne un lait parfumé et riche.
 Leurs animaux sont de races rustiques (chèvres du Rove et alpines chamoisées) ce qui leur permet de rester à l’extérieur toute l’année. Les Carel produisent des fromages fermiers : les matières premières sont exclusivement issues de l’exploitation et transformées sur place dans une fromagerie qui répond aux normes européennes.
-Des fromages de chèvre.','Des fromages de chèvre. Leurs spécialités sont :
-•Le pèbre d’aï : un crottin de chèvre de 100 gr On peut le consommer frais, à partir de 5 jours, ou affiné, de 10 à 30 jours. Il est aromatisé d’un brin de sarriette (pèbre d’aï)
-
-•La ravigotte : un fromage de chèvre à tartiner, aromatisé vec de l’ail, du basilic et de l’huile d’olive.
-
-•La tomme de la Sainte Baume : une pâte pressée, type tomme de Savoie. Elle est affinée au minimum un mois',1),
+Des fromages de chèvre.','Le pèbre d’aï : un crottin de chèvre de 100 gr On peut le consommer frais, à partir de 5 jours, ou affiné, de 10 à 30 jours. Il est aromatisé d’un brin de sarriette (pèbre d’aï) ,
+La ravigotte : un fromage de chèvre à tartiner, aromatisé vec de l’ail, du basilic et de l’huile d’olive ,La tomme de la Sainte Baume : une pâte pressée, type tomme de Savoie. Elle est affinée au minimum un mois',1),
 
 ('Brice_Hermieux','Brice','Hermieux','La campagne du Gapeau, Hyères','EN 2017, délaissant la blouse blanche et les laboratoires de microbiologie, Brice reprend l’exploitation familiale sur Hyères succédant ainsi à son grand-père métayer et son père horticulteur.
 
@@ -71,10 +64,10 @@ EN 2017 La certificat bio est obtenue pour la première fois.
 On parle d’eux :
 
 Brice Hermieux, var matin février 2018 [https://www.pressreader.com/france/var-matin-frejus-saint- raphael/20180222/282132111925157]
-','',1),
+','Oeufs ,Viandes',1),
 
 ('Bertrand_Allais','Bertrand','Allais','Adresse non renseignée','Entre Bras et Châteauvert se trouve le domaine de la Rouvière plane où Bertrand Allais cultive céréales et légumineuses depuis 1977. Converti à l’agriculture biologique depuis l’an 2000, sur les 70 hectares de son exploitation, il en cultive 60, en laisse 10 en jachères. L’irrigation se fait par le canal de l’Argens. 
-Le compost à base de fumier récupéré chez des éleveurs biologiques et de temps à autre un engrais azoté viennent nourrir ses terres déjà enrichies en azote par la culture de légumineuses telles que luzerne et sainfoin.','Il choisit de cultiver d’anciennes variétés de céréales plus adaptées au climat, plus goûteuses mais forcément moins productives : blé tendre, blé des Pharaons dit Kamut, petit épeautre, seigle… à partir desquelles il fabrique ses farines. Il produit également des pois chiches et des lentilles vertes.',1),
+Le compost à base de fumier récupéré chez des éleveurs biologiques et de temps à autre un engrais azoté viennent nourrir ses terres déjà enrichies en azote par la culture de légumineuses telles que luzerne et sainfoin. Il choisit de cultiver d’anciennes variétés de céréales plus adaptées au climat, plus goûteuses mais forcément moins productives : blé tendre, blé des Pharaons dit Kamut, petit épeautre, seigle… à partir desquelles il fabrique ses farines;','Farines , Pois chiches , Lentilles vertes',1),
 
 ('Philippe_Bilocq','Philippe','Bilocq','Domaine de Saint André, La Saulce','L’exploitation familiale, le Domaine St André, a été créée de toute pièce en 1992 dans les Hautes-Alpes. Elle se situe à 15km au sud de Gap, à 600m d''altitude au pied du massif de Céüse sur les coteaux ensoleillés dominant la Durance. Depuis plus de 15 ans, pommiers et poiriers sont cultivés ainsi que de la vigne selon les méthodes de l''Agriculture Biologique. 23 variétés de fruits (poires,coings…), dont 17 de pommes sont produites.
 
@@ -82,22 +75,22 @@ Ceci entraine un étalement de la récolte entre le 15 août au 30 octobre.
 
 Les fruits sont ramassés à la main. Un premier tri est effectué dans le champ, d’un côté les fruits qui iront en frigo et, de l’autre, ceux à transformer de suite en purées, gelées, confiture, jus de fruits et cidre.
 
-Tous les produits de l’exploitation sont certifiés AB par Ecocert.','',1),
+Tous les produits de l’exploitation sont certifiés AB par Ecocert.','Pommes , Poires , Coings , Raisin',1),
 
 ('Vincent_Mele','Vincent','Mélé','Les Sagnes, Saint Julien du Gua, Ardèche','Producteur de châtaignes, de farine de châtaignes, de confitures… Vincent Mélé est installé à Saint Julien du Gua, Ardèche, depuis 2015. 
 Par la localisation de l’exploitation dans le Parc Régional des Monts d''Ardèche, ses châtaignes bénéficient de l''Appellation d''origine protégée (AOP).
 Cette AOP est réservée aux châtaignes de variétés locales anciennes ardéchoises, cultivées en vergers de type traditionnels. Depuis 2016, tous ses châtaigniers sont en reconversion en l''agriculture biologique.
 
 Son engagement principal est de produire des fruits de grande qualité. Les quantités restent donc modestes. La production de châtaignes est de l''ordre de 3 tonnes par saison (d''octobre à novembre). 
-','Vincent Mélé travaille en famille de façon artisanale. Tout est question de respect de la châtaigneraie et du produit. 
+Vincent Mélé travaille en famille de façon artisanale. Tout est question de respect de la châtaigneraie et du produit. 
 
-•Le ramassage des châtaignes se fait au sol à la main avec un seau et une forcole. La main d''œuvre est familiale. Seules les plus belles châtaignes sont ramassées, les plus petits fruits sont laissés au sol pour nourrir les brebis. 
-•Le tri des châtaignes est aussi réalisé à la main (calibre et variété). Puis elles sont trempées dans une grande bassine d''eau pour éliminer les châtaignes véreuses. Les autres sont mises à sécher sur une grille pour les conditionner plus tard.
+Le ramassage des châtaignes se fait au sol à la main avec un seau et une forcole. La main d''œuvre est familiale. Seules les plus belles châtaignes sont ramassées, les plus petits fruits sont laissés au sol pour nourrir les brebis. 
+Le tri des châtaignes est aussi réalisé à la main (calibre et variété). Puis elles sont trempées dans une grande bassine d''eau pour éliminer les châtaignes véreuses. Les autres sont mises à sécher sur une grille pour les conditionner plus tard.
 
 
 Les principales variétés de châtaignes cultivées sont : La Comballe (ancienne variété de châtaigne très gustative et légèrement sucrée), La Bouche Rouge et la Garinche
 La farine de châtaigne est une farine sans gluten-.
-La confiture de châtaigne est dite extra car c''est le produit le plus concentré en purée de châtaigne (minimum 45 % de purée de châtaigne) par rapport à une crème de marron (38 %) et une confiture de châtaigne (35 %)',1);
+La confiture de châtaigne est dite extra car c''est le produit le plus concentré en purée de châtaigne (minimum 45 % de purée de châtaigne) par rapport à une crème de marron (38 %) et une confiture de châtaigne (35 %)','Chataîgnes ,Confiture de chataîgne',1);
 
 INSERT INTO "product" ("label") VALUES
 ('Légumes'),
