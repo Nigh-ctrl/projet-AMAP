59137808743952839941958620375ed14328d86a
Thibault <NighDev@gmail.com>
2021-08-05 15:45:16 +0200

    déconnexion téléphone

--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -1,14 +1,65 @@
 
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, useContext } from "react"
 import './style.scss';
 import logo from "../../../public/logos/round.png"
 import {Link} from "react-router-dom"
+import {ConnexionContext} from '../../ConnexionContext.js'
 
 const Header =  () =>{
   const [isActive, toggleBurger] = useState(false)
   const hidePanel = () =>{
     toggleBurger(!isActive)
   }
+  const [token, setToken] = useContext(ConnexionContext)
+
+  const logout = () =>{
+    setToken(undefined)
+    localStorage.clear('tokenStored')
+    hidePanel()
+    alert("vous êtes déconnecté")
+  }
+
+  if(token){
+    return(
+    <header>
+        <div className="left">
+                <img src={logo}/>
+        </div>
+        <div className="center">
+            <span>Amap la Seyne sur Mer</span>
+        </div>
+        <div className="right">
+          <Link to="/" > 
+            <p>Accueil</p>
+          </Link>
+          <Link to="/contact">
+            <p>Contact</p>
+          </Link>
+        </div>
+        <div onTouchEnd={() => toggleBurger(!isActive)} className={isActive ? "burger active" : "burger"} >
+          <span className="bar-1"/>
+          <span className="bar-2"/>
+          <span className="bar-3"/>
+        </div> 
+        <div className={isActive ? "panel active" : "panel"}>
+          
+          <Link to="/" onTouchEnd={hidePanel} > 
+            Accueil
+          </Link>
+          <Link to="/contact"onTouchEnd={hidePanel} >
+            Contact
+          </Link>
+           <Link to="/mentions-legales"onTouchEnd={hidePanel} >
+             Mentions légales
+           </Link>
+           <Link to="/" onTouchEnd={logout} >
+            Deconnexion
+           </Link>
+        </div> 
+    </header>
+    )
+  }
+
   return(
     <header>
         <div className="left">
